38512bed81c839dff208425f2a7de15b0f882cc5
2901shivam <shivamsingh201617@gmail.com>
2024-01-15 15:28:46 +0530

    Azure assignment

---
+++ AssignmentDay7/Controllers/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using AssignmentDay7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentDay7.Controllers
+{
+    public class PlayerController : Controller
+    {
+
+        List<Player> player = new List<Player>()
+        {
+            new Player(){PId=1,Pname="Rohit Sharma",PCountry="India",PType="Captain Batsman"},
+              new Player(){PId=2,Pname="Virat Kohli",PCountry="India",PType="Batsman"},
+                new Player(){PId=3,Pname="Shoib Akhtar",PCountry="Pakistan",PType="Fats Baowler"},
+                  new Player(){PId=4,Pname="Yuvraj Singh",PCountry="India",PType="AllRounder"},
+                    new Player(){PId=5,Pname="David Warner",PCountry="Austrelia",PType="Left hand Batsman"},
+                      new Player(){PId=6,Pname="Surya",PCountry="India",PType="Right-Hand Batsman"},
+
+        };
+        public IActionResult Index()
+        {
+            return View(player);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        [HttpPost]
+
+        public IActionResult Create(Player pl)
+        {
+            if (ModelState.IsValid)
+            {
+                player.Add(pl);
+                return RedirectToAction("Index");
+            }
+            return View(pl);
+        }
+    }
+}
---
+++ AssignmentDay7/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace AssignmentDay7.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+
+        public string Pname { get; set; }
+
+        public string PCountry { get; set; }
+
+        public string PType { get; set; }
+
+    }
+}
